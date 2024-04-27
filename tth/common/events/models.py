e34775b691186e85890555e9f46de71db1b74f22
Sergey Natalenko <sergey.natalenko@mail.ru>
2024-04-28 00:14:00 +0300

    Event API

--- tth/common/events/models.py
+++ tth/common/events/models.py
@@ -1,26 +1,77 @@
-from dataclasses import dataclass
+from collections.abc import Sequence
 from datetime import datetime
-from typing import Self
+from typing import Any, Self
 
-from tth.db.models import Event as EventDb
+from pydantic import BaseModel, ConfigDict
+
+from tth.common.models.pagination import MetaPaginationModel
+from tth.db.models import FeatureValue
 
 
-@dataclass(frozen=True)
-class Event:
+class EventModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     place_id: int
     name: str
     description: str
     started_at: datetime
     ended_at: datetime
+    created_at: datetime
+    updated_at: datetime
+
+
+class CreateEventModel(BaseModel):
+    place_id: int
+    name: str
+    description: str
+    started_at: datetime
+    ended_at: datetime
+
+
+class EventFeatureModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    name: str
+    slug: str
+    value: FeatureValue
+
+
+class EventWithFeaturesModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    place_id: int
+    name: str
+    description: str
+    started_at: datetime
+    ended_at: datetime
+    created_at: datetime
+    updated_at: datetime
+
+    features: Sequence[FeatureValue]
+
+
+class EventPaginationModel(BaseModel):
+    meta: MetaPaginationModel
+    items: Sequence[EventModel]
 
     @classmethod
-    def build(cls, obj: EventDb) -> Self:
+    def build(
+        cls,
+        limit: int,
+        offset: int,
+        total: int,
+        items: Sequence[Any],
+    ) -> Self:
         return cls(
-            id=obj.id,
-            place_id=obj.place_id,
-            name=obj.name,
-            description=obj.description,
-            started_at=obj.started_at,
-            ended_at=obj.ended_at,
+            meta=MetaPaginationModel(limit=limit, offset=offset, total=total),
+            items=[EventModel.model_validate(item) for item in items],
         )
+
+
+class UpdateEventModel(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    started_at: datetime | None = None
+    ended_at: datetime | None = None
--- tth/rest/api/v1/router.py
+++ tth/rest/api/v1/router.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-from tth.rest.api.v1.estimations import router as estimations_router
 from tth.rest.api.v1.monitoring import router as monitoring_router
 from tth.rest.api.v1.users import router as users_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(monitoring_router)
 router.include_router(users_router)
-router.include_router(estimations_router)
