f777c380b73876f21aa32ce8d4130a08973454d3
forlorn <clickshaq11@gmail.com>
2024-04-28 19:06:05 +0300

    add root page and event page

--- frontend/src/widgets/layout/index.tsx
+++ frontend/src/widgets/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 import { Header } from '@widgets/header';
 import { Outlet } from 'react-router-dom';
 
@@ -11,17 +11,7 @@ export const Layout = () => {
           width: '100%',
         }}
       >
-        <Container
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            pt: { xs: 14, sm: 20 },
-            pb: { xs: 8, sm: 12 },
-          }}
-        >
-          <Outlet />
-        </Container>
+        <Outlet />
       </Box>
     </>
   );
