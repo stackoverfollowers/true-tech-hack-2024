418ad519152d05f7ab91f309a1573e41d54dae4d
nevsky118 <nevsky118@gmail.com>
2024-04-28 22:08:13 +0300

    refactor: styles

--- frontend/src/pages/root/index.tsx
+++ frontend/src/pages/root/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, styled } from '@mui/material';
+import { Container, Typography, styled } from '@mui/material';
 import { LiveEvent } from '@widgets/live-event';
 
 const Title = styled(Typography)({
@@ -10,6 +10,32 @@ const StyledContainer = styled(Container)({
   padding: 10,
 });
 
+const Cards = styled('div')(() => ({
+  display: 'flex',
+  flexWrap: 'wrap',
+  margin: '0 -8px',
+}));
+
+const Wrapper = styled('div')(({ theme }) => ({
+  position: 'relative',
+  marginTop: theme.spacing(3),
+  width: '100%',
+  minHeight: '1px',
+  paddingInline: theme.spacing(1),
+  flex: '0 0 100%',
+  maxWidth: '100%',
+
+  [theme.breakpoints.up('sm')]: {
+    flex: '0 0 50%',
+    maxWidth: '50%',
+  },
+
+  [theme.breakpoints.up('md')]: {
+    flex: '0 0 33.333333%',
+    maxWidth: '33.333333%',
+  },
+}));
+
 export const RootPage = () => {
   return (
     <>
@@ -17,25 +43,23 @@ export const RootPage = () => {
         <Title variant="h2">Афиша Санкт-Петербурга</Title>
       </StyledContainer>
       <StyledContainer>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 6, sm: 12, md: 18 }}>
-          {Array(100)
-            .fill(undefined)
-            .map(() => (
-              <Grid item xs={4} sx={{ alignItems: 'center' }}>
-                <LiveEvent
-                  id="1"
-                  img="https://live.mts.ru/image/536x360/affinazh-russkie-pesni-s-orkestrom-narodnykh-instrumentov-09321d6d-b516-086e-12c3-1c87ef582f53.jpg"
-                  title="Концерт «Mary Gu. Club Show»"
-                  time="12 мая, 18:00"
-                  place="Гигант-Холл"
-                  price="1000р"
-                  discount="10%"
-                  accessibilityType="Bad"
-                  accessibility="Недоступно для инвалидов"
-                />
-              </Grid>
-            ))}
-        </Grid>
+        <Cards>
+          {[...Array(30)].map((_, idx) => (
+            <Wrapper key={idx}>
+              <LiveEvent
+                id={idx}
+                img="https://live.mts.ru/image/536x360/affinazh-russkie-pesni-s-orkestrom-narodnykh-instrumentov-09321d6d-b516-086e-12c3-1c87ef582f53.jpg"
+                title="Концерт «Mary Gu. Club Show»"
+                time="12 мая, 18:00"
+                place="Гигант-Холл"
+                price="1000р"
+                discount="10%"
+                accessibilityType="Bad"
+                accessibility="Недоступно для инвалидов"
+              />
+            </Wrapper>
+          ))}
+        </Cards>
       </StyledContainer>
     </>
   );
--- frontend/src/widgets/live-event/index.tsx
+++ frontend/src/widgets/live-event/index.tsx
@@ -1,52 +1,63 @@
-import { Box, Typography, styled, Chip, Stack, Paper } from '@mui/material';
+import { Box, Chip, Grow } from '@mui/material';
 import { Accessibility } from '@shared/types/accessibility';
 import { AccessibilityChip } from '@widgets/accessibility-chip';
 import { Link } from 'react-router-dom';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { clsx } from 'clsx';
+import { forwardRef, useEffect, useState } from 'react';
 
-const StyledBox = styled(Box)({
-  backgroundColor: 'transparent',
-  width: '25rem',
-});
+import styles from './styles.module.css';
 
-const StyledPaper = styled(Paper)({
-  padding: 20,
-  borderRadius: 16,
-  display: 'flex',
-  flexDirection: 'column',
-  rowGap: 20,
-});
+export const LiveEvent = forwardRef<Ref, Props>(
+  ({ id, img, title, price, discount, accessibilityType, accessibility, time, place }, ref) => {
+    const [mounted, setMounted] = useState(false);
 
-const StyledImg = styled('img')({
-  borderRadius: 16,
-  objectFit: 'contain',
-  objectPosition: '50% 50%',
-  width: '100%',
-  height: '100%',
-  overflowClipMargin: 'content-box',
-  transform: 'matrix(1, 0, 0, 1, 0, 0)',
-});
+    useEffect(() => {
+      setMounted(true);
+    }, []);
 
-const ColoredChip = styled(Chip)({
-  backgroundColor: '#7C86FE',
-  color: 'white',
-  fontSize: 12,
-  fontWeight: 500,
-});
+    return (
+      <Grow
+        in={mounted}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(mounted ? { timeout: id * 150 } : {})}
+      >
+        <Link to="#">
+          <div ref={ref} className={styles.card}>
+            <div className={styles.cover}>
+              <img src={img} className={styles.img} />
 
-const GrayChip = styled(Chip)({
-  backgroundColor: '#F2F3F7',
-  color: 'black',
-  fontWeight: 500,
-  fontSize: 12,
-});
+              <div className={styles.chips}>
+                <Chip label={discount} className={clsx(styles.chip, styles.gray)} />
+                <AccessibilityChip label={accessibility} ac={accessibilityType} />
+              </div>
+            </div>
+            <div className={styles.inner}>
+              <Box sx={{ color: 'text.secondary' }} className={styles.subtitle}>
+                {place}
+              </Box>
+              <Box sx={{ color: 'text.primary' }} className={styles.title}>
+                {title}
+              </Box>
+              <div className={styles.footer}>
+                <div className={styles.time}>
+                  <AccessTimeIcon sx={{ width: 16, height: 16 }} />
+                  <Box sx={{ color: 'text.secondary' }}>{time}</Box>
+                </div>
+                <Box sx={{ color: 'text.primary', fontWeight: 500 }}>{price}</Box>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </Grow>
+    );
+  },
+);
 
-const Labels = styled('div')({
-  columnGap: 10,
-  display: 'flex',
-});
+type Ref = HTMLDivElement;
 
-type LiveEventProps = {
-  id: string;
+type Props = {
+  id: number;
   img: string;
   title: string;
   price: string;
@@ -55,36 +66,4 @@ type LiveEventProps = {
   accessibility: string;
   time: string;
   place: string;
-};
-
-export const LiveEvent = ({
-  id,
-  img,
-  title,
-  price,
-  discount,
-  accessibilityType,
-  accessibility,
-  time,
-  place,
-}: LiveEventProps) => {
-  return (
-    <StyledBox>
-      <StyledPaper elevation={1}>
-        <Link to={`/events/${id}`}>
-          <StyledImg src={img} />
-        </Link>
-        <Labels>
-          <ColoredChip label={price} />
-          <GrayChip label={discount} />
-          <AccessibilityChip label={accessibility} ac={accessibilityType} />
-        </Labels>
-        <Stack>
-          <Typography variant="body1">{title}</Typography>
-          <Typography variant="body2">{time}</Typography>
-          <Typography variant="body2">{place}</Typography>
-        </Stack>
-      </StyledPaper>
-    </StyledBox>
-  );
 };
