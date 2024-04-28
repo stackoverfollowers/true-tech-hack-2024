b1bf078a49dfa8335cfed33b6cbcc473024f5d6f
nevsky118 <nevsky118@gmail.com>
2024-04-28 22:08:13 +0300

    refactor: styles

--- frontend/src/widgets/header/index.tsx
+++ frontend/src/widgets/header/index.tsx
@@ -1,8 +1,11 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
-import { styled } from '@mui/material';
+import { clsx } from 'clsx';
 import { Link } from 'react-router-dom';
+
+import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
 const logoStyle = {
   width: '140px',
@@ -10,63 +13,71 @@ const logoStyle = {
   cursor: 'pointer',
   padding: 5,
 };
-
-const StyledHeader = styled('header')({
-  bgcolor: 'white',
-  backgroundImage: 'none',
-  mt: 2,
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-});
-
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrolled(scrollPosition >= 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <StyledHeader>
-      <Box
-        sx={{
-          flexGrow: 1,
-          display: 'flex',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-          p: 5,
-        }}
+    <div className={styles.root}>
+      <header
+        className={clsx(styles.header, {
+          [styles.shadow]: isScrolled,
+        })}
       >
-        <Link to="/">
-          <img
-            src={'https://live.mts.ru/images/header/Logo.svg'}
-            style={logoStyle}
-            alt="logo of mts"
-          />
-        </Link>
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <MenuItem sx={{ py: '6px', px: '12px' }}>
-            <Typography variant="h6" color="text.primary">
-              Все события
-            </Typography>
-          </MenuItem>
-          <MenuItem sx={{ py: '6px', px: '12px' }}>
-            <Typography variant="h6" color="text.primary">
-              Концерты
-            </Typography>
-          </MenuItem>
-          <MenuItem sx={{ py: '6px', px: '12px' }}>
-            <Typography variant="h6" color="text.primary">
-              Театры
-            </Typography>
-          </MenuItem>
-          <MenuItem sx={{ py: '6px', px: '12px' }}>
-            <Typography variant="h6" color="text.primary">
-              Мюзиклы
-            </Typography>
-          </MenuItem>
-          <MenuItem sx={{ py: '6px', px: '12px' }}>
-            <Typography variant="h6" color="text.primary">
-              Стэндап
-            </Typography>
-          </MenuItem>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+            p: 2,
+          }}
+        >
+          <Link to="/">
+            <img
+              src={'https://live.mts.ru/images/header/Logo.svg'}
+              style={logoStyle}
+              alt="logo of mts"
+            />
+          </Link>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <MenuItem sx={{ py: '6px', px: '12px' }}>
+              <Typography variant="h6" color="text.primary">
+                Все события
+              </Typography>
+            </MenuItem>
+            <MenuItem sx={{ py: '6px', px: '12px' }}>
+              <Typography variant="h6" color="text.primary">
+                Концерты
+              </Typography>
+            </MenuItem>
+            <MenuItem sx={{ py: '6px', px: '12px' }}>
+              <Typography variant="h6" color="text.primary">
+                Театры
+              </Typography>
+            </MenuItem>
+            <MenuItem sx={{ py: '6px', px: '12px' }}>
+              <Typography variant="h6" color="text.primary">
+                Мюзиклы
+              </Typography>
+            </MenuItem>
+            <MenuItem sx={{ py: '6px', px: '12px' }}>
+              <Typography variant="h6" color="text.primary">
+                Стэндап
+              </Typography>
+            </MenuItem>
+          </Box>
         </Box>
-      </Box>
-    </StyledHeader>
+      </header>
+    </div>
   );
 };
