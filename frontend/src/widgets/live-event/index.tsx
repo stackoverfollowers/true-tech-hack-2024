2623868c1664c340ab0f370a46edf9d352e6034c
nevsky118 <nevsky118@gmail.com>
2024-05-12 13:13:40 +0300

    add place

--- frontend/src/widgets/live-event/index.tsx
+++ frontend/src/widgets/live-event/index.tsx
@@ -6,54 +6,59 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 import styles from './styles.module.css';
 import { EventsResponse } from '@shared/types/events';
+import { Place } from '@widgets/place';
 
 export const LiveEvent = forwardRef<Ref, Props>((props, ref) => {
   const [mounted, setMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
   return (
-    <Grow
-      in={mounted}
-    >
-      <Link to="#">
-        <div ref={ref} className={styles.card}>
-          <div className={styles.cover}>
-            <img src={props.image_url} className={styles.img} />
+    <>
+      <Grow
+        in={mounted}
+      >
+        <Link to="#">
+          <div ref={ref} className={styles.card}>
+            <div className={styles.cover}>
+              <img src={props.image_url} className={styles.img} />
 
-            {/* <div className={styles.chips}>
-              <Chip label={discount} className={clsx(styles.chip, styles.gray)} />
-              <AccessibilityChip label={accessibility} ac={accessibilityType} />
-            </div> */}
-          </div>
-          <div className={styles.inner}>
-            <Box sx={{ color: 'text.secondary' }} className={styles.subtitle}>
-              {props.event_type}
-            </Box>
-            <Box sx={{ color: 'text.primary' }} className={styles.title}>
-              {props.name}
-            </Box>
-            <Box sx={{ color: 'text.primary' }} className={styles.description}>
-              {props.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-            </Box>
-            <div className={styles.footer}>
-              <div className={styles.time}>
-                <CalendarMonthIcon sx={{ width: 16, height: 16 }} />
-                <Box sx={{ color: 'text.secondary' }}>{new Intl.DateTimeFormat('ru-RU', {
-                  dateStyle: 'medium',
-                }).format(new Date(props.created_at))}</Box>
-              </div>
-              <div className={styles.place}>
-                <PushPinIcon sx={{ width: 16, height: 16, color: 'text.secondary' }} />
-                <Box sx={{ color: 'text.secondary' }}>{props.place_id}</Box>
+              {/* <div className={styles.chips}>
+                <Chip label={discount} className={clsx(styles.chip, styles.gray)} />
+                <AccessibilityChip label={accessibility} ac={accessibilityType} />
+              </div> */}
+            </div>
+            <div className={styles.inner}>
+              <Box sx={{ color: 'text.secondary' }} className={styles.subtitle}>
+                {props.event_type}
+              </Box>
+              <Box sx={{ color: 'text.primary' }} className={styles.title}>
+                {props.name}
+              </Box>
+              <Box sx={{ color: 'text.primary' }} className={styles.description}>
+                {props.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+              </Box>
+              <div className={styles.footer}>
+                <div className={styles.time}>
+                  <CalendarMonthIcon sx={{ width: 16, height: 16 }} />
+                  <Box sx={{ color: 'text.secondary' }}>{new Intl.DateTimeFormat('ru-RU', {
+                    dateStyle: 'medium',
+                  }).format(new Date(props.created_at))}</Box>
+                </div>
+                <div className={styles.place} onClick={() => setIsOpen(true)}>
+                  <PushPinIcon sx={{ width: 16, height: 16, color: 'text.secondary' }} />
+                  <Box sx={{ color: 'text.secondary' }}>{props.place_id}</Box>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Link>
-    </Grow>
+        </Link>
+      </Grow>
+      <Place placeId={props.place_id} open={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 });
 
