0e43c27e8acde80b8866309d10d56327aceec043
nevsky118 <nevsky118@gmail.com>
2024-05-12 12:58:28 +0300

    events

---
+++ frontend/src/shared/types/places.ts
@@ -0,0 +1,12 @@
+type Place = {
+  id: number;
+  name: string;
+  url: string;
+  image_url: string;
+  description: null;
+  address: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type { Place };
